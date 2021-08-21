aee83148ad78211e92e21974207b0bfed9f0cb47
Mikkel Hofstedt Juul <46485458+MikkelHJuul@users.noreply.github.com>
2021-08-21 02:42:40 +0200

    Non-atomic

--- primitive/primitive.go
+++ primitive/primitive.go
@@ -2,18 +2,14 @@ package primitive
 
 import (
 	"sync"
-	"sync/atomic"
 )
-
-const ReadHere int32 = 1
-const ReadOther int32 = 0
 
 // LeftRightPrimitive provides the basic core of the leftt-right pattern.
 type LeftRightPrimitive struct {
 	// readIndicator is an array of 2 read-indicators, counting the reader numbers on the left/right instance
 	lock *sync.RWMutex
 	// readHere represents which instance to read
-	readHere *int32
+	writeJobs chan func(interface{})
 	// other is the other instance
 	other *LeftRightPrimitive
 	Data  interface{}
@@ -21,46 +17,47 @@ type LeftRightPrimitive struct {
 
 // New creates a LeftRightPrimitive
 func New(leftData interface{}, rightData interface{}) *LeftRightPrimitive {
+        writeJobs := make(chan func(interface{}))
 	r := &LeftRightPrimitive{
 		lock:          sync.RWMutex{},
-		readHere:      new(int32),
+		writeJobs:     writeJobs,
 		Data:          rightData,
 	}
 
 	l := &LeftRightPrimitive{
 		lock:          sync.RWMutex{},
-		readHere:      new(int32),
+		writeJobs:     writeJobs,
 		Data:          leftData,
 		other:         r,
 	}
 
-	*l.readHere = ReadHere
-	*r.readHere = ReadOther
 	r.other = l
 	return l // starts reading on the left side
 }
 
 // ApplyReadFn applies read operation on the chosen instance, oh, I really need generics, interface type is ugly
 func (lr *LeftRightPrimitive) ApplyReadFn(fn func(interface{})) {
-	if lr.isReader() {
+	select {
+	case wrt := <- lr.writeJobs:
+		lr.other.ApplyReadFn(fn)
+		lr.write(wrt)
+	default:
 		lr.lock.RLock()
 		fn(lr.Data)
-                lr.lock.RUlock()
-	} else {
-                lr.other.ApplyReadFn(fn)
-        }
+		lr.lock.RUlock()
+	}
 }
 
 // ApplyWriteFn applies write operation on the chosen instance, write operation is done twice, on the left and right
 // instance respectively, this might make writing longer, but the readers are wait-free.
 func (lr *LeftRightPrimitive) ApplyWriteFn(fn func(interface{})) {
-	if lr.isReader() {
-		lr.other.write(fn)
-		lr.other.startRead()
-                lr.stopRead()
+	select {
+	case wrt := <- writeJobs:
+		lr.write(wrt)
 		lr.write(fn)
-	} else {
-                lr.other.ApplyWriteFn(fn)
+		writeJobs <- fn
+	default:
+                lr.other.write(fn)
         }
 }
 
@@ -68,17 +65,4 @@ func (lr *LeftRightPrimitive) write(fn func(interface{})) {
         lr.lock.Lock()
         fn(lr.Data)
         lr.lock.Unlock()
-}
-
-func (lr *LeftRightPrimitive) startRead() {
-        atomic.StoreInt32(lr.readHere, ReadHere)
-}
-
-func (lr *LeftRightPrimitive) stopRead() {
-        atomic.StoreInt32(lr.readHere, ReadOther)
-}
-
-func (lr *LeftRightPrimitive) isReader() bool {
-        read := atomic.LoadInt32(lr.readHere)
-	return read == ReadHere
 }
