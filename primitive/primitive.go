2c78ec343ad4e0b9bdddd88d1ec17d6589a0c15a
Mikkel Juul <mju@dmi.dk>
2021-08-21 12:55:29 +0200

    code is written out in stead of recursion

--- primitive/primitive.go
+++ primitive/primitive.go
@@ -10,7 +10,7 @@ const ReadOther int32 = 0
 
 // LeftRightPrimitive provides the basic core of the leftt-right pattern.
 type LeftRightPrimitive struct {
-	// readIndicator is an array of 2 read-indicators, counting the reader numbers on the left/right instance
+	// lock protects this side
 	lock *sync.RWMutex
 	// readHere represents which instance to read
 	readHere *int32
@@ -43,25 +43,33 @@ func New(leftData interface{}, rightData interface{}) *LeftRightPrimitive {
 // ApplyReadFn applies read operation on the chosen instance, oh, I really need generics, interface type is ugly
 func (lr *LeftRightPrimitive) ApplyReadFn(fn func(interface{})) {
 	if lr.isReader() {
-		lr.lock.RLock()
-		fn(lr.Data)
-		lr.lock.RUnlock()
+		lr.read(fn)
 	} else {
-		lr.other.ApplyReadFn(fn)
+		lr.other.read(fn)
 	}
+}
+
+func (lr *LeftRightPrimitive) read(fn func(interface{})) {
+	lr.lock.RLock()
+	fn(lr.Data)
+	lr.lock.RUnlock()
 }
 
 // ApplyWriteFn applies write operation on the chosen instance, write operation is done twice, on the left and right
 // instance respectively, this might make writing longer, but the readers are wait-free.
 func (lr *LeftRightPrimitive) ApplyWriteFn(fn func(interface{})) {
 	if lr.isReader() {
-		lr.other.write(fn)
-		lr.other.startRead()
-		lr.stopRead()
-		lr.write(fn)
+		writeAndToggle(lr.other, lr, fn)
 	} else {
-		lr.other.ApplyWriteFn(fn)
+		writeAndToggle(lr, lr.other, fn)
 	}
+}
+
+func writeAndToggle(first, second *LeftRightPrimitive, fn func(interface{})) {
+	first.write(fn)
+	first.startRead()
+	second.stopRead()
+	second.write(fn)
 }
 
 func (lr *LeftRightPrimitive) write(fn func(interface{})) {
