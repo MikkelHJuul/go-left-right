96eae704ef64c908a149c0aea40e38871b216e35
Mikkel Hofstedt Juul <46485458+MikkelHJuul@users.noreply.github.com>
2021-08-21 03:22:16 +0200

    Channel read indicator
    In stead of atomic shenanigans

--- primitive/primitive.go
+++ primitive/primitive.go
@@ -9,7 +9,7 @@ type LeftRightPrimitive struct {
 	// readIndicator is an array of 2 read-indicators, counting the reader numbers on the left/right instance
 	lock *sync.RWMutex
 	// readHere represents which instance to read
-	writeJobs chan func(interface{})
+	read chan struct{}
 	// other is the other instance
 	other *LeftRightPrimitive
 	Data  interface{}
@@ -19,13 +19,13 @@ type LeftRightPrimitive struct {
 func New(leftData interface{}, rightData interface{}) *LeftRightPrimitive {
 	r := &LeftRightPrimitive{
 		lock:          sync.RWMutex{},
-		writeJobs:     make(chan func(interface{})),
+		writeJobs:     make(chan struct{}),
 		Data:          rightData,
 	}
 
 	l := &LeftRightPrimitive{
 		lock:          sync.RWMutex{},
-		writeJobs:     make(chan func(interface{})),
+		writeJobs:     make(chan struct{}),
 		Data:          leftData,
 		other:         r,
 	}
@@ -37,13 +37,12 @@ func New(leftData interface{}, rightData interface{}) *LeftRightPrimitive {
 // ApplyReadFn applies read operation on the chosen instance, oh, I really need generics, interface type is ugly
 func (lr *LeftRightPrimitive) ApplyReadFn(fn func(interface{})) {
 	select {
-	case wrt := <- lr.writeJobs:
-		lr.other.ApplyReadFn(fn)
-		lr.write(wrt)
-	default:
+	case lr.read <- lr.read:
 		lr.lock.RLock()
 		fn(lr.Data)
 		lr.lock.RUlock()
+	default:
+		lr.other.ApplyReadFn(fn)
 	}
 }
 
@@ -51,17 +50,17 @@ func (lr *LeftRightPrimitive) ApplyReadFn(fn func(interface{})) {
 // instance respectively, this might make writing longer, but the readers are wait-free.
 func (lr *LeftRightPrimitive) ApplyWriteFn(fn func(interface{})) {
 	select {
-	case wrt := <- lr.writeJobs:
-		lr.write(wrt)
-		lr.write(fn)
-		lr.other.writeJobs <- fn
+	case lr.read <- lr.read:
+		lr.other.ApplyWriteFn(fn)
 	default:
-                lr.other.write(fn)
-        }
+		lr.write(fn)
+		lr.read <- lr.other.read
+		lr.other.write(fn)
+	}
 }
 
 func (lr *LeftRightPrimitive) write(fn func(interface{})) {
-        lr.lock.Lock()
-        fn(lr.Data)
-        lr.lock.Unlock()
+	lr.lock.Lock()
+	fn(lr.Data)
+	lr.lock.Unlock()
 }
