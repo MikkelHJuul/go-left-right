be5da682e6d4f3c9730f362640a529f7bc05af2b
Mikkel Juul <mju@dmi.dk>
2021-08-21 12:45:06 +0200

    format and small stuff

--- primitive/primitive.go
+++ primitive/primitive.go
@@ -6,10 +6,10 @@ import (
 
 // LeftRightPrimitive provides the basic core of the leftt-right pattern.
 type LeftRightPrimitive struct {
-	// readIndicator is an array of 2 read-indicators, counting the reader numbers on the left/right instance
+	// readIndicator is an array of 2 readState-indicators, counting the reader numbers on the left/right instance
 	lock *sync.RWMutex
-	// readHere represents which instance to read
-	read chan struct{}
+	// readHere represents which instance to readState
+	readState chan struct{}
 	// other is the other instance
 	other *LeftRightPrimitive
 	Data  interface{}
@@ -18,49 +18,51 @@ type LeftRightPrimitive struct {
 // New creates a LeftRightPrimitive
 func New(dataInit func() interface{}) *LeftRightPrimitive {
 	r := &LeftRightPrimitive{
-		lock: new(sync.RWMutex),
-		read: make(chan struct{}, 1),
-		Data: dataInit(),
+		lock:      new(sync.RWMutex),
+		readState: make(chan struct{}, 1),
+		Data:      dataInit(),
 	}
 
 	l := &LeftRightPrimitive{
-		lock:  new(sync.RWMutex),
-		read:  make(chan struct{}, 1),
-		Data:  dataInit(),
-		other: r,
+		lock:      new(sync.RWMutex),
+		readState: make(chan struct{}, 1),
+		Data:      dataInit(),
+		other:     r,
 	}
 
 	r.other = l
-	l.read <- struct{}{}
+	l.readState <- struct{}{}
 	return l // starts reading on the left side
 }
 
-// ApplyReadFn applies read operation on the chosen instance, oh, I really need generics, interface type is ugly
+// ApplyReadFn applies readState operation on the chosen instance, oh, I really need generics, interface type is ugly
 func (lr *LeftRightPrimitive) ApplyReadFn(fn func(interface{})) {
 	select {
-	case m := <-lr.read:
-		lr.read <- m
-		lr.lock.RLock()
-		fn(lr.Data)
-		lr.lock.RUnlock()
+	case <-lr.readState:
+		lr.readState <- struct{}{}
+		lr.read(fn)
 	default:
-		lr.other.ApplyReadFn(fn)
+		lr.other.read(fn)
 	}
+}
+
+func (lr *LeftRightPrimitive) read(fn func(interface{})) {
+	lr.lock.RLock()
+	fn(lr.Data)
+	lr.lock.RUnlock()
 }
 
 // ApplyWriteFn applies write operation on the chosen instance, write operation is done twice, on the left and right
 // instance respectively, this might make writing longer, but the readers are wait-free.
 func (lr *LeftRightPrimitive) ApplyWriteFn(fn func(interface{})) {
 	select {
-	case m := <-lr.read:
-		lr.read <- m
-		lr.other.ApplyWriteFn(fn)
+	case <-lr.readState:
+		lr.other.readState <- struct{}{}
 	default:
-		lr.write(fn)
-		m := <-lr.other.read
-		lr.read <- m
-		lr.other.write(fn)
 	}
+	lr.write(fn)
+	lr.readState <- <-lr.other.readState
+	lr.other.write(fn)
 }
 
 func (lr *LeftRightPrimitive) write(fn func(interface{})) {
